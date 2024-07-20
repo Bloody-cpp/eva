4e08eee2f4a600fa5e91d763e218ceb5ee8174bc
Влад <bloodyvampir.global@gmail.com>
2024-07-20 23:42:11 +0300

    calculator

--- main.cpp
+++ main.cpp
@@ -1,5 +1,32 @@
-#include <stdio.h>
+#include <iostream>
+#include <stdint.h>
+
+using namespace std;
 
 int main(int, char**){
-    printf("Hello, from eva!\n");
+    int64_t a, b;
+    char operation;
+    while(true) {
+        int64_t result = 0;
+        cin >> a >> b >> operation;
+        switch (operation)
+        {
+        case '+':
+            result = a + b;
+            break;
+        case '-':
+            result = a - b;
+            break;
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            result = a / b;
+            break;
+        default:
+            cout << "error unknown operation" << endl;
+            break;
+        }
+        cout << "Result: " << result << endl;
+    }
 }
